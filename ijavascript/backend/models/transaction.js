9cbc629dc16c86aed161559860eecb2652c5b959
rishab.sharma <rishab.sharma@taazaa.com>
2021-10-05 19:22:23 +0530

    Remove commented code and added centralised env config

--- ijavascript/backend/models/transaction.js
+++ ijavascript/backend/models/transaction.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = mongoose.Schema({
-  userid: {type: String, required: true},
-  amount: {type: String, required: true},
-  description: {type: String, required: true},
-  date: {type: String, required:true}
+  userid: { type: String, required: true },
+  amount: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: String, required: true }
 });
-
-// userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Transaction", transactionSchema);
--- ijavascript/backend/models/user.js
+++ ijavascript/backend/models/user.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
-// const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-  userid: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  name: {type: String, required: true},
-  last_login: {type: String, required:true},
-  balance: {type: String, required:true}
+  userid: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  last_login: { type: String, required: true },
+  balance: { type: String, required: true }
 });
-
-// userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema);
